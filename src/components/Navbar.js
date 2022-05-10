a93524db07a98cf8ee649c51aa36460b2f9c447a
Ricardo <ricardomaciascastillo@hotmail.com>
2022-05-09 20:46:20 -0500

    icons mui

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,11 @@ import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../auth/authContext';
 import { types } from '../types/types';
 import { Link } from "react-router-dom";
+import PlayCircleOutlineTwoToneIcon from '@mui/icons-material/PlayCircleOutlineTwoTone';
+import LibraryMusicTwoToneIcon from '@mui/icons-material/LibraryMusicTwoTone';
+import InfoTwoToneIcon from '@mui/icons-material/InfoTwoTone';
+import ShoppingCartTwoToneIcon from '@mui/icons-material/ShoppingCartTwoTone';
+import LogoutTwoToneIcon from '@mui/icons-material/LogoutTwoTone';
 
 
 const Navbar = () => {
@@ -24,6 +29,7 @@ const Navbar = () => {
   return (
       <>
 <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
+
   
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
@@ -32,16 +38,16 @@ const Navbar = () => {
     <ul className="navbar-nav mx-3">
 
       <li className="nav-item active">
-        <Link to="/reproductor" className="nav-link">Player |</Link> 
+        <Link to="/reproductor" className="nav-link"><PlayCircleOutlineTwoToneIcon/> Player |</Link> 
       </li> 
       <li  className="nav-item">
-        <Link to="/shop" className="nav-link">Albums |</Link>
+        <Link to="/shop" className="nav-link"><LibraryMusicTwoToneIcon /> Albums |</Link>
       </li>
       <li  className="nav-item">
-        <Link to="/shop" className="nav-link">Shop |</Link>
+        <Link to="/shop" className="nav-link"><ShoppingCartTwoToneIcon />  Shop |</Link>
       </li>
       <li  className="nav-item">
-        <Link to="/about" className="nav-link">About </Link>
+        <Link to="/about" className="nav-link"><InfoTwoToneIcon /> About </Link>
       </li>
 
 
@@ -63,6 +69,7 @@ const Navbar = () => {
         className="btn btn-dark logout"
      
         >
+          <LogoutTwoToneIcon />
             Logout
         </button>
         <span className='loggedUser'> {user.name} </span>
